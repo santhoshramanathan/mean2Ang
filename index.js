7fc8756599718233cb09c665e41084d8da9fcd33
santhoshramanathan <santhoshramanathan25@gmail.com>
2018-01-28 17:37:33 +0530

    server-setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config=require('./conifg/database');
+const path = require('path');
+
+mongoose.Promise=global.Promise;
+
+mongoose.connect(config.uri,(err)=>{
+    if(err){
+        console.log('Could not connect to database '+err);
+    }else{
+        console.log('Connect to database '+config.db);
+    }
+});
+
+app.use(express.static(__dirname+'/client/dist/'));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+});
+
+app.listen(8080,()=>{
+    console.log('Listening on port 8080');
+
+});
